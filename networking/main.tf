d19aefd1826e540713dd6d123fc3f72f484006fe
gopal <gopal1409@gmail.com>
2025-10-17 13:10:19 +0530

    terraform module

--- networking/main.tf
+++ networking/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-  name = "${local.resource_name_prefix}-${var.vnet_name}" #sap-dev-vnet-gopal
+  name = var.vnet_name #sap-dev-vnet-gopal
   #this vnet need to be mapped with rg name and location
   location            = azurerm_resource_group.kyn-rg.location
   resource_group_name = azurerm_resource_group.kyn-rg.name
